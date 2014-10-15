aa801cc4e971bbfee0a2080093ff3a8a7dd5164c
pierre <pierre@appsfire.com>
2014-10-15 11:49:18 +0200

    Add a simple init test

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,8 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should have 3 exchange rates and no transactions', function () {
+    expect(scope.exchangeRates.length).toBe(3);
+    expect(scope.transactions.length).toBe(0);
   });
 });
